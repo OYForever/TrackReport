ce46eca4b820970064673f158a7d5c886e10e97b
欧阳芳斌 <478027478@qq.com>
2025-05-21 17:22:36 +0800

    fix: 获取window问题

--- TrackReport/src/Tool/AppVersionCheckTool.swift
+++ TrackReport/src/Tool/AppVersionCheckTool.swift
@@ -114,7 +114,6 @@ extension AppVersionCheckTool {
     static func kKeyWindow() -> UIWindow? {
         if #available(iOS 13.0, *) {
             return UIApplication.shared.connectedScenes
-                .filter { $0.activationState == .foregroundActive }
                 .first(where: { $0 is UIWindowScene })
                 .flatMap({ $0 as? UIWindowScene })?.windows
                 .first(where: { $0.isKeyWindow })
